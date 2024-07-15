b2de4ee4e2bfb8f9394b11651abd876222ea762e
Karolina Mikolajczyk <karolina.kujawska@interia.pl>
2024-07-15 13:26:27 +0200

    Add API

---
+++ Ainimals.io.PayNow/IPayNowApi.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Common;
+using System.Runtime.Versioning;
+using RestEase;
+
+namespace Ainimals.io.PayNow;
+
+public interface IPayNowApi
+
+{
+   
+    [Post("/v1/payments")]
+   Task  PaymentRequest();
+
+
+    
+    [Get("/v1/payments/{paymentId}/status")]
+    Task PaymentStatus();
+
+
+    [Get("/v2/payments/paymentmethods")]
+    Task PaymentMethods();
+
+    [Get("/v1/payments/dataprocessing/notices")]
+    Task GetCDPRClauses();
+
+    [Post("/v1/payments/{paymentId}/refunds")]
+    Task RefundRequest();
+
+    [Get("/v1/refunds/{refundId}/status")]
+    Task RefundStatus();
+
+    [Post("/v1/refunds/{refundId}/cancel")]
+    Task CancelRefund();
+
+}
