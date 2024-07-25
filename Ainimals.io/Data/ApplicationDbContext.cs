1e827b03a590219dffaa0fa8763493fcddbef0c2
Karolina Mikolajczyk <karolina.kujawska@interia.pl>
2024-07-25 13:08:50 +0200

    add migration

--- Ainimals.io/Data/ApplicationDbContext.cs
+++ Ainimals.io/Data/ApplicationDbContext.cs
@@ -8,18 +8,27 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Order> Orders { get; set; }
+    // public DbSet<PaymentState> PaymentStates { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.Entity<Order>()
-            .HasOne(x => x.User)
-            .WithMany(x => x.Orders)
-            .HasForeignKey(x => x.UserId)
-            .IsRequired();
+            .HasKey(x => x.Id)
+            .HasName("PK_OrderId");
         builder.Entity<Order>()
-            .Property(x => x.PaymentStatus)
-            .HasConversion(v => v.ToString(),
-                v => (PaymentStatus)Enum.Parse(typeof(PaymentStatus), v));
+        .HasOne(x => x.User)
+        .WithMany(x => x.Orders)
+        .HasForeignKey(x => x.UserId)
+        .IsRequired();
+        // builder.Entity<Order>()
+        //     .Property(x => x.PaymentStatus)
+        //     .HasConversion(v => v.ToString(),
+        //         v => (PaymentStatus)Enum.Parse(typeof(PaymentStatus), v));
+        // builder.Entity<PaymentState>()
+        //     .HasNoKey()
+        //     .Property(x=>x.PaymentStatus)
+        //     .HasConversion(v=>v.ToString(), 
+        //         v=>(PaymentStatus)Enum.Parse(typeof(PaymentStatus),v));
         
         base.OnModelCreating(builder);
     }
